f51746374891b5e1184daeda89fc4234844193ea
Adam Hussain <Adam Hussain>
2017-03-06 10:34:18 -0500

    done: Added a error message for when AJAX request for API fails

--- js/script.js
+++ js/script.js
@@ -16,20 +16,20 @@ function initWeatherAPI() {
         console.log(data.weather[0].main);
         console.log(data.weather[0].main);
         temp = ((data.main.temp * 9)/5) + 32 ;
-        $( "#forcast" ).html( "<p>Main:"+data.weather[0].main+"</p><p>Description:"
+        $( "#forecast" ).html( "<p>Main:"+data.weather[0].main
+        +"</p><p>Description:"
         +"<img src ='http://openweathermap.org/img/w/"+data.weather[0].icon+".png'>"
-        +data.weather[0].description+
-        "</p>"+"<p>Temperature:"+temp+"</p>");
+        +data.weather[0].description+"</p>"
+        +"<p>Temperature:"+temp+"</p>");
 
       },
       error: function() {
 
         console.log("Error accessing API");
+        $( "#forecast" ).html("Error accessing API");
 
       }
 
       });
-
   });
-
 }
